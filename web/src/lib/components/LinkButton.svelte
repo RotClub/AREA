9f9f3b404593341d57e7447277ee847db44fd209
Kapsulon <theo.bary@epitech.eu>
2024-09-17 09:38:13 +0200

    ADD: navbar + font

---
+++ web/src/lib/components/LinkButton.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	export let href: string = '';
+</script>
+
+{#if ($page.url.pathname === '/dashboard' && href === '/dashboard') || (href !== '/dashboard' && $page.url.pathname.includes(href))}
+	<div class="relative h-full flex items-center">
+		<a {href} class="text-primary-500 h-full flex items-center justify-center">
+			<slot />
+		</a>
+		<span class="absolute h-1 w-full bottom-0 left-0 bg-primary-500 active rounded-t-sm"></span>
+	</div>
+{:else}
+	<div class="relative h-full flex items-center">
+		<a {href} class="h-full flex items-center justify-center"><slot /></a>
+	</div>
+{/if}
+
+<style>
+	.active {
+		view-transition-name: active-page;
+	}
+</style>
--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -4,11 +4,12 @@
 	// Highlight JS
 	import hljs from 'highlight.js/lib/core';
 	import 'highlight.js/styles/github-dark.css';
-	import { storeHighlightJs } from '@skeletonlabs/skeleton';
+	import { initializeStores, storeHighlightJs } from '@skeletonlabs/skeleton';
 	import xml from 'highlight.js/lib/languages/xml'; // for HTML
 	import css from 'highlight.js/lib/languages/css';
 	import javascript from 'highlight.js/lib/languages/javascript';
 	import typescript from 'highlight.js/lib/languages/typescript';
+	import { onNavigate } from '$app/navigation';
 
 	hljs.registerLanguage('xml', xml); // for HTML
 	hljs.registerLanguage('css', css);
@@ -16,10 +17,23 @@
 	hljs.registerLanguage('typescript', typescript);
 	storeHighlightJs.set(hljs);
 
+	initializeStores();
+
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+	onNavigate((navigation) => {
+		if (!document.startViewTransition) return;
+
+		return new Promise<void>((resolve) => {
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
 </script>
 
 <slot />
