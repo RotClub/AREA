c46fb6960a31394b309ff6345856d91862de64b4
Kapsulon <theo.bary@epitech.eu>
2024-10-01 10:20:51 +0200

    ADD: nodes in workspace tab

--- web/src/lib/components/Workspace/Node.svelte
+++ web/src/lib/components/Workspace/Node.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
-	import { Cog, Play, Plus } from "lucide-svelte";
+	import { Cog, Play, Plus, Trash2 } from "lucide-svelte";
 
 	export let action: string;
 </script>
 
-<div class="card w-full h-[10rem] flex flex-col overflow-hidden">
+<div class="card w-full h-[10rem] flex-shrink-0 flex flex-col overflow-hidden">
 	<div class="flex flex-col justify-between h-full">
 		<div class="flex flex-row items-center p-4">
 			<span class="text-xl font-semibold text-secondary-300">On: </span>
@@ -13,12 +13,15 @@
 				<button class="btn-icon text-surface-200">
 					<Cog />
 				</button>
+				<button class="btn-icon text-error-500">
+					<Trash2 />
+				</button>
 				<button class="btn-icon text-success-500">
 					<Play fill="#3de184" />
 				</button>
 			</div>
 		</div>
-		<button class="bg-primary-500 w-full h-6 flex flex-row justify-center items-center">
+		<button class="bg-primary-500 w-full h-6 flex flex-row justify-center items-center hover:bg-primary-700 transition-colors">
 			<span>
 				<Plus />
 			</span>
---
+++ web/src/routes/auth/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { EvervaultCard } from "$lib/components/Aceternity/EvervaultCard";
+    import { onMount } from "svelte";
+
+    let loaded: boolean = false;
+
+    onMount(() => {
+        loaded = true;
+    });
+</script>
+
+<style>
+    .splitter.loaded {
+        width: 80% !important;
+        transition: width .75s;
+    }
+</style>
+
+<div class="w-full h-3/4 flex flex-col justify-center items-center">
+    <div class="relative container mx-auto max-w-md h-[30rem] space-y-4">
+        <div class="flex flex-row justify-center items-center gap-6">
+            <img src="/icon.svg" alt="icon" class="w-24 h-24" />
+			<span class="font-semibold text-5xl">Frispy</span>
+        </div>
+        <div class="card h-[30rem] max-w-md flex flex-col">
+            <span class="w-full flex flex-row items-center justify-center text-xl mt-4">Authentication</span>
+            <div class="w-full h-2 flex flex-row items-center justify-center">
+                <span class="w-[0%] border-b-2 splitter" class:loaded={loaded}></span>
+            </div>
+        </div>
+    </div>
+</div>
