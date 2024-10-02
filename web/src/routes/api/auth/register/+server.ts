1c6be2c3358723c020fabfabcc9f9811961afa1a
Yanis Moyon <yanis.moyon@epitech.eu>
2024-10-02 18:03:37 +0200

    FIX: debug log + prisma disconnect + response formatting

--- web/src/routes/api/auth/register/+server.ts
+++ web/src/routes/api/auth/register/+server.ts
@@ -1,6 +1,5 @@
-import {createJWTToken, encryptPWD, verifyJWTAuth} from "$lib/auth";
-import {UserRole} from "@prisma/client";
-import {PrismaClient} from "@prisma/client";
+import { PrismaClient, UserRole } from "@prisma/client";
+import { createJWTToken, encryptPWD } from "$lib/auth";
 import { checkAPIAccess } from "$lib/api";
 
 export const POST = async ({ request, url }) => {
@@ -9,12 +8,12 @@ export const POST = async ({ request, url }) => {
 	// Validate the token + user role
 	const { valid, err } = await checkAPIAccess(client, request, url, UserRole.ADMIN);
 	if (!valid) {
+		client.$disconnect();
 		return new Response(JSON.stringify({ error: err }), {
 			status: 400,
 			headers: { "Content-Type": "application/json" },
 		});
 	}
-
 	try {
 		const body = await request.json();
 
@@ -47,13 +46,11 @@ export const POST = async ({ request, url }) => {
 			status: 200,
 			headers: { "Content-Type": "application/json" },
 		});
-		console.log(res);
 		client.$disconnect();
 		return res;
 	} catch (error) {
-		console.error("Error creating user:", error);
 		client.$disconnect();
-		return new Response(JSON.stringify({ error: "Failed to create user" }), {
+		return new Response(JSON.stringify({ error: "Failed to create user: " + error }), {
 			status: 500,
 			headers: { "Content-Type": "application/json" },
 		});
