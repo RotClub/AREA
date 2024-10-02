4fcce8f6df3264799ed6e19fbf76f38a1fdca1fe
Yanis Moyon <yanis.moyon@epitech.eu>
2024-10-02 18:29:58 +0200

    ADD: load on /auth for authentication

---
+++ web/src/routes/auth/+page.server.ts
@@ -0,0 +1,30 @@
+import {type Actions, fail, redirect} from "@sveltejs/kit";
+import {encryptPWD} from "$lib/auth";
+import {PrismaClient} from "@prisma/client";
+
+export const actions = {
+	default: async (event) => {
+		const data = await event.request.formData();
+		const email = data.get("email");
+		const password = encryptPWD(String(data.get("password")));
+		const client = new PrismaClient();
+
+		try {
+			if (!email) {
+				return fail(400, { error: "Email is required" });
+			}
+			const user = await client.user.findUnique({
+				where: { email: String(email), hashedPassword: password },
+			});
+			client.$disconnect();
+			if (!user) {
+				return fail(401, { error: "Email or password invalid" });
+			}
+			event.cookies.set("token", user.token || "", { path: "/", httpOnly: false });
+		} catch (error) {
+			client.$disconnect();
+			return fail(500, { error: "Failed to log in" });
+		}
+		return redirect(301, "/dashboard");
+	}
+} satisfies Actions;
