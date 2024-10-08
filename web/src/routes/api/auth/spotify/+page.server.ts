afa959372b7c8e10fd9d76d21c0731fae7f321a2
Yanis Moyon <yanis.moyon@epitech.eu>
2024-10-08 14:34:12 +0200

    ADD: spotify auth workflow w/o data persistence

---
+++ web/src/routes/api/auth/spotify/+page.server.ts
@@ -0,0 +1,31 @@
+import { adaptUrl } from '$lib/api'
+import { redirect } from "@sveltejs/kit";
+import queryString from 'query-string';
+import { PrismaClient } from "@prisma/client";
+
+function generateRandomString(length: number) {
+	let text = '';
+	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+	for (let i = 0; i < length; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
+}
+
+export const load = async () => {
+	const state = generateRandomString(16);
+	const scope = 'user-read-private user-read-email';
+
+	return redirect(
+		300,
+		`https://accounts.spotify.com/authorize?` + queryString.stringify({
+			response_type: 'code',
+			client_id: process.env.SPOTIFY_CLIENT_ID,
+			scope: scope,
+			redirect_uri: `${adaptUrl()}/api/link/spotify`,
+			state: state,
+			show_dialog: true
+		})
+	)
+}
