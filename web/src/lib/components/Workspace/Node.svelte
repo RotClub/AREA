69acb5bcd510a10a68cfd7cea420809eb65882e3
Kapsulon <theo.bary@epitech.eu>
2024-10-08 11:50:58 +0200

    RM: actions column

--- web/src/lib/components/Workspace/Node.svelte
+++ web/src/lib/components/Workspace/Node.svelte
@@ -2,11 +2,13 @@
 	import { Cog, Play, Plus, Trash2 } from "lucide-svelte";
 
 	export let action: string;
+	export let meta: { [key: string]: string } = {};
+	export let edit: boolean;
 </script>
 
-<div class="card w-full h-[10rem] flex-shrink-0 flex flex-col overflow-hidden">
-	<div class="flex flex-col justify-between h-full">
-		<div class="flex flex-row items-center p-4">
+<div class="card w-full flex-shrink-0 flex flex-col overflow-hidden">
+	<div class="flex flex-col justify-between h-full p-2">
+		<div class="flex flex-row items-center px-2">
 			<span class="text-xl font-semibold text-secondary-300">On: </span>
 			<span class="ml-2">{action}</span>
 			<div class="flex flex-row flex-grow justify-end">
@@ -21,11 +23,32 @@
 				</button>
 			</div>
 		</div>
-		<button
-			class="bg-primary-500 w-full h-6 flex flex-row justify-center items-center hover:bg-primary-700 transition-colors">
-			<span>
-				<Plus />
-			</span>
-		</button>
+		<div class="flex flex-row overflow-x-scroll">
+			{#each Object.entries(meta) as [key, value]}
+				<div class="flex flex-col items-center justify-center px-4 py-2 shrink-0">
+					<span class="text-secondary-300 text-xl shrink-0 text-nowrap">{key}</span>
+					<span class="shrink-0 text-nowrap">{value}</span>
+				</div>
+			{/each}
+		</div>
+		<div class="w-full flex flex-col alternate">
+			<slot />
+		</div>
 	</div>
+	{#if edit}
+	<button
+		class="bg-primary-500 w-full h-6 flex flex-row justify-center items-center hover:bg-primary-700 transition-colors">
+		<span>
+			<Plus />
+		</span>
+	</button>
+	{/if}
 </div>
+
+<style>
+	.alternate:nth-child(even) {
+		background-color: rgb(var(--color-surface-500));
+		border-left: 1px solid rgb(var(--color-surface-600));
+		border-right: 1px solid rgb(var(--color-surface-600));
+	}
+</style>
