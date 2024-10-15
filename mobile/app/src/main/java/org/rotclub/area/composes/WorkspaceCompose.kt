125ecdf37fa0c7b5e4a9f5d12e9cc7eb2eff140a
PaulArbez <paul.arbez@epitech.eu>
2024-10-15 16:18:33 +0200

    FIX: warnings on profile and workspace pages

--- mobile/app/src/main/java/org/rotclub/area/composes/WorkspaceCompose.kt
+++ mobile/app/src/main/java/org/rotclub/area/composes/WorkspaceCompose.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -22,7 +21,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableIntState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -43,7 +41,7 @@ import org.rotclub.area.fontFamily
 @Composable
 fun ColumnCard(navController: NavController, title: String, text: String) {
     var titleState by remember { mutableStateOf(title) }
-    var textState by remember { mutableStateOf(text) }
+    val textState by remember { mutableStateOf(text) }
 
     Column(
         modifier = Modifier
