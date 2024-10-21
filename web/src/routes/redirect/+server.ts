7a284948989e33f868378e36b3472a7680326a60
Yanis Moyon <yanis.moyon@epitech.eu>
2024-10-21 19:02:18 +0200

    ADD: redirect route

---
+++ web/src/routes/redirect/+server.ts
@@ -0,0 +1,9 @@
+import { redirect } from "@sveltejs/kit";
+
+export const GET = async ({ url }) => {
+	const redurl = url.searchParams.get("new_url");
+	if (!redurl) {
+		return redirect(301, "/dashboard");
+	}
+	return redirect(301, redurl);
+}
