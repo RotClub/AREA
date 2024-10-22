a1a5135163f10b4a9c0bff1d21ae7276c67b3d07
Yanis Moyon <yanis.moyon@epitech.eu>
2024-10-22 13:46:25 +0200

    ADD: discord oauth2 wf

---
+++ web/src/routes/oauth/discord/link/+page.server.ts
@@ -0,0 +1,15 @@
+import { adaptUrl } from "$lib/api";
+import { redirect } from "@sveltejs/kit";
+import queryString from "query-string";
+
+export const load = async () => {
+	return redirect(
+		300,
+		"https://discord.com/oauth2/authorize?" + queryString.stringify({
+			client_id: process.env.DISCORD_CLIENT_ID,
+			response_type: "code",
+			redirect_uri: `${adaptUrl()}/oauth/discord/callback`,
+			integration_type: 1,
+			scope: "identify email openid messages.read connections"
+		}))
+};
