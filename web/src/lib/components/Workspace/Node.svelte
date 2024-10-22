6673c6609ac28a67405758fac81749945ed16878
Kapsulon <theo.bary@epitech.eu>
2024-10-22 13:55:37 +0200

    ADD: delete button on nodes

--- web/src/lib/components/Workspace/Node.svelte
+++ web/src/lib/components/Workspace/Node.svelte
@@ -1,21 +1,85 @@
 <script lang="ts">
+	import { getDisplayNameFromId, getIconPathFromId } from "$lib/services";
 	import { Cog, Play, Plus, Trash2 } from "lucide-svelte";
+	import { type ModalSettings, type ModalStore, getModalStore } from "@skeletonlabs/skeleton";
+	import { parse as cookieParser } from "cookie";
+
+	let modalStore: ModalStore = getModalStore();
 
 	export let action: string;
 	export let meta: { [key: string]: string } = {};
 	export let edit: boolean;
+	export let programId: number;
+	export let actionId: number;
+	export let programs: Array<{
+		name: string;
+		id: number;
+		nodeAmount: number;
+	}>;
+	export let loaded: boolean;
+
+	async function openAddReactionModal() {
+		new Promise<boolean>((resolve) => {
+			const modal: ModalSettings = {
+				type: "component",
+				component: "addReactionModalComponent",
+				response: (r: boolean) => {
+					resolve(r);
+				}
+			};
+
+			modalStore.trigger(modal);
+		}).then(async (r) => {
+			if (r) {
+				loaded = false;
+				await window.fetch(`/api/programs/${programId}/node`, {
+					method: "PUT",
+					headers: {
+						"Content-Type": "application/json",
+						Authorization: `Bearer ${cookieParser(document.cookie)["token"]}`
+					},
+					body: JSON.stringify({
+						isReaction: true,
+						id: r,
+						metadata: {}
+					})
+				});
+				programs = await (await window.fetch("/api/programs")).json();
+				console.log(programs);
+				loaded = true;
+			}
+		});
+	}
+
+	async function deleteNode() {
+		loaded = false;
+		await window.fetch(`/api/programs/${programId}/node`, {
+			method: "DELETE",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${cookieParser(document.cookie)["token"]}`
+			},
+			body: JSON.stringify({
+				isAction: true,
+				id: actionId
+			})
+		});
+		programs = await (await window.fetch("/api/programs")).json();
+		loaded = true;
+	}
 </script>
 
 <div class="card w-full flex-shrink-0 flex flex-col overflow-hidden">
 	<div class="flex flex-col justify-between h-full p-2">
 		<div class="flex flex-row items-center px-2">
-			<span class="text-xl font-semibold text-secondary-300">On: </span>
-			<span class="ml-2">{action}</span>
+			<span class="text-xl font-semibold text-secondary-300 mr-3">On: </span>
+			<img src={getIconPathFromId(action)} alt={action} width="24px" />
+			<span class="ml-2">{getDisplayNameFromId(action)}</span>
 			<div class="flex flex-row flex-grow justify-end">
 				<button class="btn-icon text-surface-200">
 					<Cog />
 				</button>
-				<button class="btn-icon text-error-500">
+				<button class="btn-icon text-error-500" on:click={deleteNode}>
 					<Trash2 />
 				</button>
 				<button class="btn-icon text-success-500">
@@ -37,6 +101,7 @@
 	</div>
 	{#if edit}
 		<button
+			on:click={openAddReactionModal}
 			class="bg-primary-500 w-full h-6 flex flex-row justify-center items-center hover:bg-primary-700 transition-colors">
 			<span>
 				<Plus />
--- web/src/lib/services.ts
+++ web/src/lib/services.ts
@@ -1,13 +1,13 @@
 import { PrismaClient, Provider } from "@prisma/client";
 
-export const Actions: [
+export const Actions: Array<
 	{
 		service: Provider;
 		displayName: string;
 		iconPath: string;
 		actions: [{ id: string; displayName: string; meta: object }?];
-	}?
-] = [
+	}
+> = [
 	{
 		service: Provider.SPOTIFY,
 		displayName: "Spotify",
@@ -23,6 +23,20 @@ export const Actions: [
 		]
 	}
 ];
+
+export function getIconPathFromId(id: string): string {
+	const [service] = id.split(":");
+	const action = Actions.find((s) => s.service === service);
+	return action ? action.iconPath : '';
+}
+
+export function getDisplayNameFromId(id: string): string {
+	const [service, action] = id.split(":");
+	const foundService = Actions.find((s) => s.service === service);
+	if (!foundService) return '';
+	const foundAction = foundService.actions?.find((a) => a?.id === action);
+	return foundAction ? foundAction.displayName : '';
+}
 
 export async function GetAccessibleActions(jwt: string | undefined) {
 	if (!jwt) return [];
@@ -43,7 +57,6 @@ export async function GetAccessibleActions(jwt: string | undefined) {
 
 	const accessibleActions = [];
 	for (const service of user.Service) {
-		// @ts-expect-error 'action' is possibly 'undefined'. ts(18048)
 		const actions = Actions.find((action) => action.service === service.providerType);
 		if (actions) accessibleActions.push(actions);
 	}
