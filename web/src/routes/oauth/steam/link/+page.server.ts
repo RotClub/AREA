ef46e293d99e82f6ff39940e9e324f1c9e7fe517
Yanis Moyon <yanis.moyon@epitech.eu>
2024-10-23 15:58:54 +0200

    FIX: migrated steam out of API routes

--- web/src/routes/oauth/steam/link/+page.server.ts
+++ web/src/routes/oauth/steam/link/+page.server.ts
@@ -1,9 +1,10 @@
 import { redirect } from '@sveltejs/kit';
+import { adaptUrl } from "$lib/api";
 
 export const load = async () => {
   const steamOpenIdUrl = 'https://steamcommunity.com/openid/login';
-  const returnUrl = 'http://localhost:8081/api/link/steam';
-  const realm = 'http://localhost:8081';
+  const returnUrl = `${adaptUrl()}/oauth/steam/callback`;
+  const realm = adaptUrl();
 
   const authUrl = `${steamOpenIdUrl}?openid.ns=http://specs.openid.net/auth/2.0&openid.mode=checkid_setup&openid.return_to=${encodeURIComponent(returnUrl)}&openid.realm=${encodeURIComponent(realm)}&openid.identity=http://specs.openid.net/auth/2.0/identifier_select&openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select`;
 
