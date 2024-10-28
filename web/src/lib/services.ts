1e13023f39ff12bb5ccbee8fd3a747d5e1f42daa
Yanis Moyon <yanis.moyon@epitech.eu>
2024-10-28 11:46:58 +0100

    ADD: addProvider & removeProvider function (replacing API /link + /unlink)

--- web/src/lib/services.ts
+++ web/src/lib/services.ts
@@ -1,4 +1,5 @@
-import { PrismaClient, Provider } from "@prisma/client";
+import { PrismaClient, Provider, UserRole } from "@prisma/client";
+import { checkAccess } from "$lib/api";
 
 export interface ActionMetaDataType {
 	id: string;
@@ -159,4 +160,142 @@ export function getProviderTitle(provider: Provider) {
 		default:
 			return "Unknown";
 	}
+}
+
+export async function addProvider(provider: string, data: Record<string, unknown>, request: Request): Promise<Response> {
+	const providerList = Object.values(Provider).map((p) => String(p || "").toLowerCase());
+	if (!providerList.includes(provider || "")) {
+		return new Response(JSON.stringify({ error: "Invalid provider" }), {
+			status: 400,
+			headers: {
+				"Content-Type": "application/json"
+			}
+		});
+	}
+	const correctProvider = Object.values(Provider).find(
+		(p) => String(p).toLowerCase() === provider || ""
+	);
+	const client = new PrismaClient();
+	const access = await checkAccess(client, request, UserRole.USER);
+	if (!access.valid) {
+		client.$disconnect();
+		return new Response(JSON.stringify({ error: access.err }), {
+			status: 403,
+			headers: {
+				"Content-Type": "application/json"
+			}
+		});
+	}
+	const token = request.headers.get("Authorization")?.replace("Bearer ", "");
+	const user_id = await client.user.findUnique({
+		where: {
+			token: token
+		},
+		select: {
+			id: true
+		}
+	});
+	if (!user_id) {
+		client.$disconnect();
+		return new Response(JSON.stringify({ error: "User not found" }), {
+			status: 404,
+			headers: {
+				"Content-Type": "application/json"
+			}
+		});
+	}
+	const delete_service = await client.service.deleteMany({
+		where: {
+			userId: user_id.id,
+			providerType: correctProvider
+		}
+	});
+	if (!delete_service) {
+		client.$disconnect();
+		return new Response(JSON.stringify({ error: "Could not unlink user from service" }), {
+			status: 500,
+			headers: {
+				"Content-Type": "application/json"
+			}
+		});
+	}
+	if (correctProvider && data) {
+		await client.user.update({
+			where: {
+				token: token
+			},
+			data: {
+				Service: {
+					create: {
+						providerType: correctProvider,
+						metadata: data as Record<string, string>
+					}
+				}
+			}
+		});
+	}
+	client.$disconnect();
+	return new Response(`Provider ${provider} was successfully added to user`, {
+		status: 200,
+		headers: {
+			"Content-Type": "application/json"
+		}
+	});
+}
+
+export async function removeProvider(provider: string, request: Request): Promise<Response> {
+	const providerList = Object.values(Provider).map((p) => String(p).toLowerCase());
+	if (!providerList.includes(provider || "")) {
+		return new Response(JSON.stringify({ error: "Invalid provider" }), {
+			status: 400,
+			headers: {
+				"Content-Type": "application/json"
+			}
+		});
+	}
+	const correctProvider = Object.values(Provider).find(
+		(p) => String(p).toLowerCase() === provider || ""
+	);
+	const client = new PrismaClient();
+	const access = await checkAccess(client, request, UserRole.USER);
+	if (!access.valid) {
+		client.$disconnect();
+		return new Response(JSON.stringify({ error: access.err }), {
+			status: 403,
+			headers: {
+				"Content-Type": "application/json"
+			}
+		});
+	}
+	const token = request.headers.get("Authorization")?.replace("Bearer ", "");
+	const user_id = await client.user.findUnique({
+		where: {
+			token: token
+		},
+		select: {
+			id: true
+		}
+	});
+	if (!user_id) {
+		client.$disconnect();
+		return new Response(JSON.stringify({ error: "User not found" }), {
+			status: 404,
+			headers: {
+				"Content-Type": "application/json"
+			}
+		});
+	}
+	await client.service.deleteMany({
+		where: {
+			userId: user_id.id,
+			providerType: correctProvider
+		}
+	});
+	client.$disconnect();
+	return new Response(`Provider ${provider} was successfully remove from user`, {
+		status: 200,
+		headers: {
+			"Content-Type": "application/json"
+		}
+	});
 }
