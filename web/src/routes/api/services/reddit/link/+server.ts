106ab5f72ef53f0c23e215c261f9b27a99b2f9ef
Yanis Moyon <yanis.moyon@epitech.eu>
2024-11-01 19:54:39 +0100

    ADD: deeplink feature for Reddit

--- web/src/routes/api/services/reddit/link/+server.ts
+++ web/src/routes/api/services/reddit/link/+server.ts
@@ -1,8 +1,15 @@
 import { adaptUrl } from "$lib/api";
 import queryString from "query-string";
+import { getPlatformType } from "$lib/cross";
 
 export const GET = async (event) => {
 	const token = event.request.headers.get("Authorization")?.replace("Bearer ", "");
+	const platform = getPlatformType(event.request)
+	const state = JSON.stringify({
+		jwt: token,
+		user_agent: platform
+	})
+
 	if (!process.env.REDDIT_CLIENT_ID) {
 		return new Response(JSON.stringify({ error: "No Reddit client ID provided" }), {
 			headers: {
@@ -16,7 +23,7 @@ export const GET = async (event) => {
 		queryString.stringify({
 			client_id: process.env.REDDIT_CLIENT_ID,
 			response_type: "code",
-			state: token,
+			state: state,
 			redirect_uri: `${adaptUrl()}/api/services/reddit/callback`,
 			duration: "permanent",
 			scope: "identity edit history modposts modwiki privatemessages read submit subscribe vote wikiedit wikiread"
