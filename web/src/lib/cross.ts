28c49fac46a5b0c53e3e0ede270dac381f2d82ff
Yanis Moyon <yanis.moyon@epitech.eu>
2024-11-01 19:42:50 +0100

    ADD: functions and data types related to user platform

---
+++ web/src/lib/cross.ts
@@ -0,0 +1,27 @@
+import { adaptUrl } from "$lib/api";
+
+export enum PlatformType {
+	Android = "Android",
+	Web = "Web"
+}
+
+export function getPlatformType(request: Request) : PlatformType {
+	const userAgent = request.headers.get('user-agent') || '';
+
+	if (/Android/i.test(userAgent) && /wv/.test(userAgent)) {
+		return PlatformType.Android;
+	} else {
+		return PlatformType.Web;
+	}
+}
+
+export function getRedirectionURL(type: PlatformType) : string {
+	switch (type) {
+		case PlatformType.Android:
+			return "https://area-app.vercel.app/app/redirect/";
+		case PlatformType.Web:
+			return `${adaptUrl()}/dashboard`;
+		default:
+			return "";
+	}
+}
