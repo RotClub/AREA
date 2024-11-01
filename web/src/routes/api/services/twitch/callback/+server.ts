1a7fdfa8827117a4164f94537c9924a2b050710d
Yanis Moyon <yanis.moyon@epitech.eu>
2024-11-01 19:46:00 +0100

    ADD: deeplink feature for Twitch

--- web/src/routes/api/services/twitch/callback/+server.ts
+++ web/src/routes/api/services/twitch/callback/+server.ts
@@ -1,12 +1,15 @@
 import { adaptUrl } from "$lib/api";
 import { error, redirect } from "@sveltejs/kit";
 import { addProvider } from "$lib/provider";
+import { getPlatformType, getRedirectionURL, PlatformType } from "$lib/cross";
 
 export const GET = async (event) => {
 	const code = event.url.searchParams.get("code");
 	const err_msg = event.url.searchParams.get("error_description");
 	const err = event.url.searchParams.get("error");
-	const token = event.url.searchParams.get("state");
+	const state = JSON.parse(event.url.searchParams.get("state"));
+	const user_agent = state.user_agent
+	const token = state.jwt
 
 	if (!token) {
 		error(400, "No token provided");
@@ -40,5 +43,5 @@ export const GET = async (event) => {
 		const ans = await res.json();
 		error(400, ans.error);
 	}
-	return redirect(301, `${adaptUrl()}/dashboard`);
+	return redirect(301, getRedirectionURL(PlatformType[user_agent as keyof typeof PlatformType]));
 };
--- web/src/routes/api/services/twitch/link/+server.ts
+++ web/src/routes/api/services/twitch/link/+server.ts
@@ -1,8 +1,14 @@
 import { adaptUrl } from "$lib/api";
 import queryString from "query-string";
+import { getPlatformType } from "$lib/cross";
 
 export const GET = async (event) => {
 	const token = event.request.headers.get("Authorization")?.replace("Bearer ", "");
+	const platform = getPlatformType(event.request)
+	const state = JSON.stringify({
+		jwt: token,
+		user_agent: platform
+	})
 
 	if (!process.env.TWITCH_CLIENT_ID) {
 		return new Response(JSON.stringify({ error: "No Twitch client ID provided" }), {
@@ -19,7 +25,7 @@ export const GET = async (event) => {
 			response_type: "code",
 			redirect_uri: `${adaptUrl()}/api/services/twitch/callback`,
 			scope: "channel:manage:schedule channel:read:subscriptions channel:bot analytics:read:games analytics:read:extensions",
-			state: token,
+			state: state,
 			force_verify: true
 		});
 	return new Response(JSON.stringify({ url: authorizationUrl }), {
